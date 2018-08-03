268b231922f19b2104e0ea116eea1e725f5a9e60
Chris <chris@cypheros.co>
2018-08-02 22:48:44 -0400

    Initial bringup for aoscp
    Change-Id: If3eef2f1f3369b3c2897658017db9482028dc950

--- bacon.mk
+++ bacon.mk
@@ -17,8 +17,7 @@
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay
 
 # Audio
 PRODUCT_COPY_FILES += \
@@ -66,10 +65,6 @@ PRODUCT_PACKAGES += \
     init.qcom.usb.rc \
     init.recovery.bacon.rc \
     ueventd.bacon.rc
-
-# Vendor security patch level
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.lineage.build.vendor_security_patch=2016-09-01
 
 # Call the proprietary setup
 $(call inherit-product, vendor/oneplus/bacon/bacon-vendor.mk)
